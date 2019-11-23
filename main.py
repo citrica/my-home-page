67f90815f83f1a3c85427ae528af11cd0b225422
citrica <inma.rueda28@gmail.com>
2019-11-23 20:28:08 +0100

    Learnig GET an POST request

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/about")
+@app.route("/about", methods=["GET"])
 def about_me():
     return render_template("about.html")
 
@@ -27,9 +27,23 @@ def fakebook():
 def boogle():
     return render_template("boogle.html")
 
+
 @app.route("/portfolio/hair-salon")
 def hairsalon():
     return render_template("hair-salon.html")
+
+
+@app.route("/contact", methods=["POST"])
+def contact():
+    contact_name = request.form.get("contact-name")
+    contact_email = request.form.get("contact-email")
+    contact_message = request.form.get("contact-message")
+
+    print(contact_name)
+    print(contact_email)
+    print(contact_message)
+
+    return render_template("success.html")
 
 
 if __name__ == '__main__':
